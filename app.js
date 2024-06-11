4d83515fd06c7b2664cbe507550131e5d7d5b9a3
tavicon <grchsm11@hotmail.com>
2024-06-10 18:53:14 -0600

    ok

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.listen(port, function () {
+	console.log("Running port ", port);
+	console.log("Escribe una linea que imprima tu nombre abajo de esta.");
+        OK
+})
