db596f8dda8a4441444b49c1e61fd5ff556514ba
Daniel Bueno <danielbuenosan@gmail.com>
2024-06-11 16:16:39 -0600

    Cambios

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const port = 8080;
 
 app.listen(port, function () {
 	console.log("Running port ", port);
-<<<<<<< HEAD
-	console.log("Janeth Araceli López Segundo");
+	
 
-=======
 	console.log("Escribe una linea que imprima tu nombre abajo de esta.");
 	console.log("Luis Felipe Cabrera Morales");
 	console.log("Lucio Ramirez");
->>>>>>> 2f291c19f90322d75e03f7ffb281e68d9c77f40d
+	console.log("Daniel Bueno");
+	console.log("Janeth Araceli López Segundo");
 })
