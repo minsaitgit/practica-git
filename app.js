d78395a14cf1b1df1afc61e1bcfc0048a3c72836
Kristian Ortiz lemus <kristian@192.168.1.9>
2024-07-02 11:00:31 -0700

    Cambios de Kristian Ortiz

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const port = 8080;
 
 app.listen(port, function () {
 	console.log("Running port ", port);
-	console.log("Escribe una linea que imprima tu nombre abajo de esta.");
+	console.log("Kristian Ortiz Lemus");
 	
 })
