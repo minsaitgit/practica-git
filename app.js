0ad9acdf35ccd75fb577e5074279b885e8aee145
Adrián Leal <73041058+AdLe44@users.noreply.github.com>
2024-08-09 10:12:34 -0700

    Cambios de Adrián Leal (#38)
    * Cambios de Adrián Leal
    
    * Cambios de Joshua-Lopez
    
    ---------
    
    Co-authored-by: Joshua Lopez Dominguez <joshua.antonio.lpz.dmngz@gmail.com>
    Co-authored-by: JoshuaLopezDominguez <77797620+JoshuaLopezDominguez@users.noreply.github.com>

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ app.listen(port, function () {
 	console.log("Running port ", port);
 	console.log("José Alberto Trejo Pérez");
 	console.log("Escribe una linea que imprima tu nombre abajo de esta.");
-	console.log("Joshua Antonio López Domínguez.")
+	console.log("Joshua Antonio López Domínguez.");
+	console.log("Manuel Adrián Millán Leal");
 })
