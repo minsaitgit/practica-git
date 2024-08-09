8cd51c19485702ffce09696730bb3a338283ffd9
faortega <faortega2511@gmail.com>
2024-08-09 11:24:20 -0600

    Cambios de antonio-ortega (#46)

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.listen(port, function () {
 	console.log("Cristopher Cerrillo Callejas");
 	console.log("José Alberto Trejo Pérez");
 	console.log("Escribe una linea que imprima tu nombre abajo de esta.");
+	console.log("Antonio Ortega");
 	console.log("Joshua Antonio López Domínguez.");
 	console.log("Manuel Adrián Millán Leal");
 })
